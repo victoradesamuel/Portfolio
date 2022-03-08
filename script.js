eeebc9ce70ea0d1772106ccd7b2d99b98985bf45
Victor Ayomide Ade-Samuel <victoradesamuel16@gmail.com>
2022-03-08 14:04:34 +0100

    Add-mobile-menu functionality using javascript

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function openNav() {
-    document.getElementById("mobile-nav").style.width = "100%";
-  }
-  
+  document.getElementById('mobile-nav').style.width = '100%';
+}
 function closeNav() {
-    document.getElementById("mobile-nav").style.width = "0%";
-    document.querySelectorAll("nav-links").style.width = "0%";
-  }
+  document.getElementById('mobile-nav').style.width = '0%';
+}
+
+openNav();
+closeNav();
