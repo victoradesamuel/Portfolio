aa08401811232362341ec307e7e174d38b96a51f
Victor Ayomide Ade-Samuel <victoradesamuel16@gmail.com>
2022-03-08 10:30:40 +0100

    Add dynamics usig javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0%";
+    document.querySelectorAll("nav-links").style.width = "0%";
+  }
