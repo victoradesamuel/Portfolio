6e544065448eb24dd20d32df1030decb5640b6b0
Victor Ayomide Ade-Samuel <victoradesamuel16@gmail.com>
2022-03-08 23:16:11 +0100

    modify mobile menu

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function openNav() {
   mobileMenuToggleClass(document.querySelector('.mobile-nav-content'), 'mobile-toggle')
 }
 function closeNav() {
-  document.getElementById('mobile-nav').style.width = '0%';
+  document.getElementById('mobile-nav').style.display = 'none';
   mobileMenuToggleClass(document.querySelector('.mobile-toggle'), 'mobile-nav-content')
 
 }
