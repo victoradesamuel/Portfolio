b90bbcdc048eb632516490843f954c0786e27f1b
Victor Ayomide Ade-Samuel <victoradesamuel16@gmail.com>
2022-03-12 15:09:35 +0100

    Add modal popup

---
+++ popup.js
@@ -0,0 +1,153 @@
+const popup = [
+  {
+    name: 'Tonic',
+    author: 'CANOPY',
+    stack: 'Backend Dev',
+    year: 2015,
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: './assets/images/Snapshoot-Portfolio-3.png',
+    tools: ['html', 'css', 'javascript'],
+    live: 'github.com/victoradesamuel',
+    source: 'github.com/victoradesamuel',
+  },
+  {
+    name: 'Multi-Post Stories',
+    author: 'FACEBOOK',
+    stack: 'Full Stack Dev',
+    year: 2015,
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    featuredImage: './assets/images/Snapshoot-Portfolio-4.png',
+    tools: ['html', 'css', 'javascript'],
+    live: 'github.com/victoradesamuel',
+    source: 'github.com/victoradesamuel',
+  },
+  {
+    name: 'Tonic',
+    author: 'FACEBOOK 360',
+    stack: 'Full Stack Dev',
+    year: 2015,
+    description: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    featuredImage: './assets/images/Snapshoot-Portfolio.png',
+    tools: ['html', 'css', 'javascript'],
+    live: 'github.com/victoradesamuel',
+    source: 'github.com/victoradesamuel',
+  },
+  {
+    name: 'Uber Navigation',
+    author: 'Uber',
+    stack: 'Lead Developer',
+    year: 2015,
+    description: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    featuredImage: './assets/images/Snapshoot-Portfolio-2.png',
+    tools: ['html', 'css', 'javascript'],
+    live: 'github.com/victoradesamuel',
+    source: 'github.com/victoradesamuel',
+  },
+];
+
+document.querySelector('.works').innerHTML = popup
+  .map(
+    (currentProject) => `
+    <section class="project">
+<div class="project-img">
+<img
+src= ${currentProject.featuredImage}
+class="snapshoot"
+alt="Tonic project image"
+/>
+</div>
+<div class="project-description">
+<h2 class="project-title">${currentProject.name}</h2>
+<ul class="project-author">
+<li>${currentProject.author}</li>
+<li>${currentProject.stack}</li>
+<li>${currentProject.year}</li>
+</ul>
+<p class="project-text"
+>${currentProject.description}</p
+>
+<ul class="project-tools">
+<li>${currentProject.tools[0]}</li>
+<li>${currentProject.tools[1]}</li>
+<li>${currentProject.tools[2]}</li>
+</ul>
+<button type="button" class="btn">See Project</button>
+</div>
+</section>
+    `,
+  )
+  .join('');
+
+for (let i = 0; i <= popup.length; i += 1) {
+  if (i % 2 === 1) {
+    document.querySelectorAll('.project')[i].classList.add('order');
+  }
+}
+
+document.querySelector('.modals').innerHTML = popup
+  .map(
+    (currentProject) => `
+<div class="modal">
+<div class="modal-container"> 
+  <div class="modal-title">
+<h2 class="project-title">${currentProject.name}</h2>
+    <div class="close">
+  <i class="fa fa-times fa-1x"></i>
+    </div>
+</div>
+  <ul class="project-author">
+    <li>${currentProject.author}</li>
+    <li>${currentProject.stack}</li>
+    <li>${currentProject.year}</li>
+  </ul>
+  <div class="project-img">
+    <img
+      src= ${currentProject.featuredImage}
+      class="snapshoot"
+      alt="Tonic project image"
+    />
+  </div>
+  <div class="modal-description">
+    <p class="project-text">${currentProject.description}</p>
+    <div class="modal-tools">
+      <ul class="project-tools">
+        <li>${currentProject.tools[0]}</li>
+        <li>${currentProject.tools[1]}</li>
+        <li>${currentProject.tools[2]}</li>
+      </ul>
+      <span></span>
+      <div class="buttons">
+        <a href="${currentProject.live}" class="modal-button" target="_blank" rel="noopener noreferrer">See Live <img
+src="./assets/images/Icon.png"
+alt="live site icon" class="modal-img"/></a><a href="${currentProject.source}" class="modal-button" target="_blank" rel="noopener noreferrer">See Source <img
+src="./assets/images/github.png"
+alt="github icon" class="modal-img"
+/></a>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>`,
+  )
+  .join('');
+
+// event listeners for opening modal on button click
+const modalButtons = Array.from(document.querySelectorAll('.btn'));
+const modals = Array.from(document.querySelectorAll('.modal'));
+
+const modalButtonZip = modalButtons.map((button, i) => [button, modals[i]]);
+
+modalButtonZip.forEach((pair) => {
+  pair[0].addEventListener('click', () => {
+    pair[1].style.display = 'block';
+  });
+});
+
+// event listener for closing modal on button click
+document.querySelectorAll('.close').forEach((close) => {
+  close.addEventListener('click', () => {
+    document.querySelectorAll('.modal').forEach((modal) => {
+      modal.style.display = 'none';
+    });
+  });
+});
--- script.js
+++ script.js
@@ -1,256 +1,120 @@
-const mobileMenuToggleClass = (element, toggleClass) => {
-  element.classList.toggle(toggleClass);
-};
-
-function openNav() {
-  document.getElementById('mobile-nav').style.width = '100%';
-  mobileMenuToggleClass(document.querySelector('.mobile-nav-content'), 'mobile-toggle');
-}
-function closeNav() {
-  document.getElementById('mobile-nav').style.display = 'none';
-  mobileMenuToggleClass(document.querySelector('.mobile-toggle'), 'mobile-nav-content');
+function toggleMenu() {
+  document.querySelectorAll('.menu-bar').forEach((element) => element.addEventListener('click', () => {
+    document.querySelector('.mobile-menu').classList.toggle('active');
+  }));
 }
 
-openNav();
-closeNav();
+toggleMenu();
 
+function introSectionObserver() {
+  const header = document.querySelector('header');
+  const introSection = document.querySelector('.divider');
 
-
-const popup = [
-  {
-    id: 'project-1',
-    name: 'Tonic',
-    job: {
-      author: 'Canopy',
-      stack: 'Back End Dev',
-      year: '2015',
-    },
-    description: {
-      summary:
-        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-
-      portfolio:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
-    },
-    image: {
-      mobile: './assets/images/Snapshoot-Portfolio.png',
-      desktop:
-        './assets/images/SnapshootPortfolio.png',
-    },
-    tools: {
-      html: 'html',
-      css: 'css',
-      javascript: 'javascript',
-    },
-    section: 'section-img-left',
-  },
-  {
-    id: 'project-2',
-    name: 'Multi Stories Post',
-    job: {
-      author: 'Facebook',
-      stack: 'Fullstack',
-      year: '2015',
-    },
-    description: {
-      summary:
-        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-      portfolio:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
-    },
-    image: {
-      mobile: './assets/images/Snapshoot-Portfolio-1.png',
-      desktop: './assets/images/SnapshootPortfolio1.png',
-    },
-    tools: {
-      html: 'html',
-      ror: 'ruby on rails',
-      css: 'css',
-      javascript: 'javascript',
-    },
-    section: 'section-img-right',
-  },
-  {
-    id: 'project-3',
-    name: 'Facebook 360',
-    job: {
-      author: 'Facebook',
-      stack: 'Full Stack',
-      year: '2015',
-    },
-    description: {
-      summary:
-        'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-      portfolio:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
-    },
-    image: {
-      mobile: './assets/images/Snapshoot-Portfolio-2.png',
-      desktop: './assets/images/SnapshootPortfolio2.png',
-    },
-    tools: {
-      html: 'html',
-      ror: 'ruby on rails',
-      css: 'css',
-      javascript: 'javascript',
-    },
-    section: 'section-img-left',
-  },
-  {
-    id: 'project-4',
-    name: 'Uber Navigation',
-    job: {
-      author: 'Uber',
-      stack: 'Lead Developer',
-      year: '2008',
-    },
-    description: {
-      summary:
-        'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-      portfolio:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
-    },
-    image: {
-      mobile: './assets/images/Snapshoot-Portfolio-3.png',
-      desktop: './assets/images/SnapshootPortfolio3.png',
-    },
-    tools: {
-      html: 'html',
-      ror: 'ruby on rails',
-      css: 'css',
-      javascript: 'javascript',
-    },
-    section: 'section-img-right',
-  },
-];
-
-
-// pop up
-
-const Portfolio = document.querySelector('work-samples');
-const popupModal = document.querySelector('modals');
-
-Portfolio.classList.add('projects');
-
-Array.from(Portfolio.children).forEach((child, index) => {
-  child.innerHTML = `
-    <section class="${popup[index].section}">
-      <ul>
-        <li><img src="${popup[index].image.mobile}" alt="icon" class="img-1-1" /></li>
-        <li><img src="${popup[index].image.desktop}" alt="icon" class="img-1-2" /></li>
-      </ul>
-      <div class="block-1">
-        <div class="text-group">
-          <h2 class="lg-text-2">${popup[index].name}</h2>
-          <div class="client-info">
-            <h3 class="client">${popup[index].job.author}</h3>
-            <a href="#"><img src="assets/Icons/Counter.svg" alt="counter" /></a>
-            <h3 class="role">${popup[index].job.stack}</h3>
-            <a href="#"><img src="assets/Icons/Counter.svg" alt="counter" /></a>
-            <h3 class="year-label">${popup[index].job.year}</h3>
-          </div>
-        </div>
-        <h3 class="sm-text-2">${popup[index].description.summary}</h3>
-        <ul class="tags">
-          <li>${popup[index].tools.html}</li>
-          <li>${popup[index].tools.css}</li>
-          <li>${popup[index].tools.javascript}</li>
-        </ul>
-  
-        <div class="actions" id="${popup[index].id}">
-          <button class="btn" id="btn" type="button">See Project</button>
-        </div>
-      </div>
-    </section>`;
-});
-
-Array.from(Portfolio.children).forEach((item, index) => {
-  item.firstElementChild.lastElementChild.lastElementChild.firstElementChild.addEventListener(
-    'click',
-    () => {
-      popupModal.innerHTML = `
-      <!-- Modal content -->
-      <section class="modal-content">
-        <div class="popup-text-group">
-          <div class="text-group">
-            <h2 class="popup-text-lg">${popup[index].name}</h2>
-            <div class="client-info">
-              <h3 class="client">${popup[index].job.author}</h3>
-              <a href="#"><img src="assets/Icons/Counter.svg" alt="counter" /></a>
-              <h3 class="role">${popup[index].job.stack}</h3>
-              <a href="#"><img src="assets/Icons/Counter.svg" alt="counter" /></a>
-              <h3 class="year-label">${popup[index].job.year}</h3>
-            </div>
-          </div>
-          <span class="close">&times;</span>
-        </div>
-        <ul>
-          <li><img src="${popup[index].image.mobile}" alt="icon" class="img-1-1" /></li>
-          <li><img src="${popup[index].image.desktop}" alt="icon" class="img-1-2 popup-img" /></li>
-        </ul>
-        <div class="popup-bottom">
-          <p class="popup-text-sm">${popup[index].description.portfolio}</p>
-          <div class="popup-block">
-            <ul class="tags ${popup[index].id}">
-            </ul>
-            <div class="popup-actions">
-              <button class="btn btnImg" id="btn" type="button">See Live
-                <img src="assets/Icons/Icon@2x.svg" alt="icon" id="imgIcon" />
-              </button>
-              <button class="btn btnImg" id="btn" type="button">See Source
-                <img src="assets/Icons/Icon-GitHub.svg" alt="icon" id="imgIcon" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </section>
-      `;
-
-      const ulList = document.querySelector('.popup-block .tags');
-
-      Object.keys(popup[index].tools).forEach((e) => {
-        ulList.innerHTML += `<li>${popup[index].tools[e]}</li>`;
-      });
-
-      popupModal.style.display = 'block';
-      const span = document.getElementsByClassName('close')[0];
-      span.addEventListener('click', () => {
-        popupModal.style.display = 'none';
-      });
-      window.addEventListener('click', (e) => {
-        if (e.target === popupModal) {
-          popupModal.style.display = 'none';
-        }
-      });
-    },
-  );
-});
-
-const Form = document.querySelector('.form-valid');
-const emailInput = document.querySelector('.email');
-const validation = document.querySelector('.message-validation');
-
-Form.addEventListener('submit', (event) => {
-  const stringMessage = emailInput.value;
-  if (/[A-Z]/.test(stringMessage)) {
-    validation.innerHTML = 'Error! Your submission was unsuccessful, please input email in lowercase.';
-    event.preventDefault();
-  }
-});
-
-const Forms = document.querySelector('.form');
-
-Forms.addEventListener('input', () => {
-  const data = {
-    name: document.querySelector('#name').value,
-    email: document.querySelector('.email').value,
-    message: document.querySelector('#message').value,
+  const options = {
+    rootMargin: '0px',
   };
 
-  localStorage.setItem('FormData', JSON.stringify(data));
-});
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add('header-active');
+      } else {
+        header.classList.remove('header-active');
+      }
+    });
+  }, options);
 
-const formObject = JSON.parse(localStorage.getItem('FormData'));
-document.querySelector('#name').value = formObject.name;
-document.querySelector('.email').value = formObject.email;
-document.querySelector('#message').value = formObject.message;
+  observer.observe(introSection);
+}
+introSectionObserver();
+
+function portfolioSectionObserver() {
+  const portfolioNavlink = document.querySelector('#portfolio-link');
+  const portfolioSection = document.querySelector('#portfolio');
+
+  const options = {
+    rootMargin: '-280px',
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        portfolioNavlink.classList.add('active-link');
+      } else {
+        portfolioNavlink.classList.remove('active-link');
+      }
+    });
+  }, options);
+
+  observer.observe(portfolioSection);
+}
+portfolioSectionObserver();
+
+function aboutSectionObserver() {
+  const aboutNavlink = document.querySelector('#about-link');
+  const aboutSection = document.querySelector('#about');
+
+  const options = {
+    rootMargin: '-280px',
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        aboutNavlink.classList.add('active-link');
+      } else {
+        aboutNavlink.classList.remove('active-link');
+      }
+    });
+  }, options);
+
+  observer.observe(aboutSection);
+}
+aboutSectionObserver();
+
+function contactSectionObserver() {
+  const contactNavlink = document.querySelector('#contact-link');
+  const contactSection = document.querySelector('#contact');
+
+  const options = {
+    rootMargin: '-280px',
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        contactNavlink.classList.add('active-link');
+      } else {
+        contactNavlink.classList.remove('active-link');
+      }
+    });
+  }, options);
+
+  observer.observe(contactSection);
+}
+contactSectionObserver();
+
+function toggleArrows() {
+  const lists = Array.from(document.querySelectorAll('.lists'));
+  const angleIcon = Array.from(document.querySelectorAll('.angle-icon'));
+
+  const listZip = angleIcon.map((btn, index) => [btn, lists[index]]);
+
+  listZip.forEach((pair) => {
+    pair[1].style.display = 'block';
+
+    pair[0].addEventListener('click', () => {
+      if (pair[1].style.display === 'block' || pair[1].style.display === 'flex') {
+        pair[1].style.display = 'none';
+        pair[0].classList.add('rotate-angle-icon');
+      } else if (window.innerWidth > 992) {
+        pair[1].style.display = 'flex';
+        pair[0].classList.remove('rotate-angle-icon');
+      } else {
+        pair[1].style.display = 'block';
+        pair[0].classList.remove('rotate-angle-icon');
+      }
+    });
+  });
+}
+toggleArrows();
